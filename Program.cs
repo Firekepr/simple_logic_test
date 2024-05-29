a086dcce06ce2a456c0f307a1e238f3a16e57d41
Firekepr <93929735+Firekepr@users.noreply.github.com>
2024-05-29 12:43:26 -0300

    New initial action logic and loop

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@ using AppActions;
 class Program {
     static void Main(string[] args) {
         Console.WriteLine("Welcome to a simple logical test system");
+        Console.WriteLine("Select:");
 
-        InitialActions.getAction();
+        int initial_action = 0;
+
+        do {                
+            try {
+
+                initial_action = InitialActions.getAction();
+
+                if (initial_action > 2) throw new InvalidOperationException("Error: initial action");
+
+            } catch (System.Exception) {                
+                Console.Clear();
+                initial_action = 0;
+                Console.WriteLine("Invalid options, try again:");
+            }    
+        } while (initial_action == 0);
+
+        if (initial_action == 2) Environment.Exit(0);
     }
 }
--- classes/AppActions.cs
+++ classes/AppActions.cs
@@ -1,7 +1,6 @@
 namespace AppActions {
     class InitialActions {
-        public static int getAction () {
-            Console.WriteLine("Select:");
+        public static int getAction () {            
             Console.WriteLine("1 - Start:\n2 - Exit:");
 
             return Convert.ToInt32(Console.ReadLine());
