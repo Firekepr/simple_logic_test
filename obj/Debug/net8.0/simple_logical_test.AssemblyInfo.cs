3f2076345c4de08e3ee645a7dedf3710f4e6b4c9
Firekepr <93929735+Firekepr@users.noreply.github.com>
2024-05-29 12:27:17 -0300

    Some C# confugration

--- obj/Debug/net8.0/simple_logical_test.AssemblyInfo.cs
+++ obj/Debug/net8.0/simple_logical_test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simple_logical_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4321abec6912fadf81ec3d034f779844c2176d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8a58750106eef487012ca2e7dc6f11ec3fcad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("simple_logical_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simple_logical_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
