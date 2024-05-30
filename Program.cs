b4521f6447daf29075abe282d91d4d93e04cc8ee
Firekepr <93929735+Firekepr@users.noreply.github.com>
2024-05-30 18:13:22 -0300

    New user action hanlder

--- Program.cs
+++ Program.cs
@@ -4,24 +4,37 @@ using AppActions;
 class Program {
     static void Main(string[] args) {
         Console.WriteLine("Welcome to a simple logical test system");
-        Console.WriteLine("Select:");
 
-        int initial_action = 0;
+        int action = 0;
 
         do {                
             try {
 
-                initial_action = InitialActions.getAction();
+                action = UserActions.getInitialAction();
 
-                if (initial_action > 2) throw new InvalidOperationException("Error: initial action");
+                if (action > 2) throw new InvalidOperationException("Error: initial action");
 
             } catch (System.Exception) {                
                 Console.Clear();
-                initial_action = 0;
+                action = 0;
                 Console.WriteLine("Invalid options, try again:");
             }    
-        } while (initial_action == 0);
+        } while (action == 0);
 
-        if (initial_action == 2) Environment.Exit(0);
+        if (action == 2) Environment.Exit(0);
+
+        Console.Clear();
+
+        int user_action = 0;
+        
+        do {
+            user_action = UserActions.getAction();
+
+            if (user_action == 1) {  
+                int val1 = UserActions.getValue("Inform the first value (Choose between 1 to 8)");
+                int val2 = UserActions.getValue("Inform the second value (Choose between 1 to 8)");
+            }
+
+        } while (user_action != 3);
     }
 }
--- classes/AppActions.cs
+++ classes/AppActions.cs
@@ -1,9 +1,55 @@
-namespace AppActions {
-    class InitialActions {
-        public static int getAction () {            
-            Console.WriteLine("1 - Start:\n2 - Exit:");
+    namespace AppActions {
+        class UserActions {
+            public static int getInitialAction () {            
+                Console.WriteLine("1 - Start\n2 - Exit");
 
-            return Convert.ToInt32(Console.ReadLine());
+                return Convert.ToInt32(Console.ReadLine());
+            }
+
+            public static int getAction () {            
+                Console.WriteLine("Select:\n1 - Connect\n2 - Query\n3 - Exit");
+
+                int val = 0;
+
+                do {                
+                    try {
+                        val = Convert.ToInt32(Console.ReadLine());
+
+                        if (val > 3) throw new InvalidOperationException("Error: initial action");
+
+                    } catch (System.Exception) {                
+                        Console.Clear();
+                        val = 0;
+                        Console.WriteLine("Invalid options, try again:");
+                    }    
+                } while (val == 0);
+
+                return val;
+            }
+
+            public static int getValue(string actionName) {
+                int val = 0;
+                bool isValid = false;
+
+                do {                
+                    try {
+                        Console.WriteLine(actionName);   
+                        val = Convert.ToInt32(Console.ReadLine());
+
+                        if (val != 0 && val <= 8) {
+                            isValid = true;
+                        } else {
+                            Console.Clear();
+                            Console.WriteLine("Value cannot be zero or great than eight, try again:");
+                        }
+
+                    } catch (System.Exception) {                
+                        Console.Clear();
+                        Console.WriteLine("Invalid value, try again:");
+                    }    
+                } while (!isValid);
+
+                return val;
+            }
         }
     }
-}
